0f4607d47655d739bae4d5d37dd4ab4163b96db7
nezarreall-hash <nezarreall@gmail.com>
2026-06-08 09:05:46 +0100

    Add files via upload

---
+++ mikro-orm.config.js
@@ -0,0 +1,14 @@
+﻿const { MySqlDriver } = require('@mikro-orm/mysql');
+require('dotenv').config();
+
+module.exports = {
+  driver: MySqlDriver,
+  dbName: process.env.DB_NAME || 'api_database',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PWD || '',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT, 10) || 3306,
+  entities: ['./src/entities/**/*.ts'],
+  entitiesTs: ['./src/entities/**/*.ts'],
+  debug: true
+};
